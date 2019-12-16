38b3d0c8e43bb86228cbf19aea3c40d5416c788e
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-12-16 23:24:25 +0100

    Configure image loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import MyImage from '../../assets/images/my_image.jpg';
 import styles from './styles.module.scss';
 
 const App = ({ title }) => (
   <div className={styles.wrapper}>
     <h2 className={styles.title}>{title}</h2>
+    <img src={MyImage} alt="torchlight in the sky" />
   </div>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,12 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
     ],
   },
   resolve: {
