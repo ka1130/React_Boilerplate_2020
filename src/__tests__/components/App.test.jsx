a3b3e72260b9a3668aee519f6a8122e08d08a241
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-12-18 23:29:44 +0100

    Basic Jest setup

---
+++ src/__tests__/components/App.test.jsx
@@ -0,0 +1,7 @@
+import App from 'components/App/App';
+
+describe("App component", () => {
+  it('foo', () => {
+    expect(1+1).toEqual(2);
+  });
+});
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MyImage from '../../assets/images/my_image.jpg';
 import styles from './styles.module.scss';
 
@@ -8,5 +9,9 @@ const App = ({ title }) => (
     <img src={MyImage} alt="torchlight in the sky" />
   </div>
 );
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default App;
