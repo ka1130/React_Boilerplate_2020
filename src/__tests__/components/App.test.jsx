2b46daf76f725ef332b04334ccad87515451bd02
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-12-19 23:46:24 +0100

    Fix jest config

--- src/__tests__/components/App.test.jsx
+++ src/__tests__/components/App.test.jsx
@@ -1,7 +1,12 @@
-import App from 'components/App/App';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../components/App';
 
 describe("App component", () => {
   it('foo', () => {
+    const wrapper = shallow(<App title="Hello" />);
+    expect(wrapper.length).toEqual(1);
+    console.log(wrapper.debug());
     expect(1+1).toEqual(2);
   });
 });
